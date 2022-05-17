5626e55da6f2f275aba12715ea0b8f12a96a5aa3
Fábio Mori <fsmmori@gmail.com>
2022-05-17 10:19:25 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import { StatusBar, SafeAreaView, View } from 'react-native';
+import Cesta from './src/telas/Cesta';
+import mock from './src/mocks/cesta';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+
+export default function App() {
+  const [fonteCarregada]=useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,  
+  });
+
+  if (!fonteCarregada){
+    return <AppLoading />
+  }
+
+  return (
+    <SafeAreaView style={{flex:1}}>
+      <StatusBar/>
+      <Cesta {...mock}/>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
